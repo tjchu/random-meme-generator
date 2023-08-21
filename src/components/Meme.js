d0b7bafc24a24f84fe16f37985a54b21725a5f52
Terry Chu <terrychu2002@gmail.com>
2023-08-21 16:10:29 +0800

    update

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -10,6 +10,30 @@ function Meme() {
 
     const memesArray = memesData.data.memes
 
+    function handleTopTextChange(event) {
+        setMeme(
+            function(prevMeme){
+                return {
+                    ...prevMeme,
+                    topText: event.target.value,
+                }
+            }
+        );
+        console.log('top text is:', event.target.value);
+      }
+
+      function handleBottomTextChange(event) {
+        setMeme(
+            function(prevMeme){
+                return {
+                    ...prevMeme,
+                    bottomText: event.target.value,
+                }
+            }
+        );
+        console.log('bottom text is:', event.target.value);
+      }
+
     function handleClick() {
         setMeme(
             function(prevMeme) {
@@ -19,8 +43,7 @@ function Meme() {
                     newURL = memesArray[randomNum].url
                 }
                 return {
-                    topText: "",
-                    bottomText: "",
+                    ...prevMeme,
                     imgURL: newURL,
                 }
             }
@@ -30,12 +53,14 @@ function Meme() {
     return (
         <div className="App-hero">
             <div className="inputs">
-                <input type="text" className="input-box" placeholder="Shut up" />
-                <input type="text" className="input-box" placeholder="and take my money" />
+                <input type="text" className="input-box" placeholder="Shut up" onChange={handleTopTextChange} />
+                <input type="text" className="input-box" placeholder="and take my money" onChange={handleBottomTextChange} />
             </div>
             <button className="generate-button">
                 <div className="generate-button-text" onClick={handleClick}>Get a new meme image  🖼</div>
             </button>
+            <h2>{meme.topText}</h2>
+            <h2>{meme.bottomText}</h2>
             <img className="meme-image" src={meme.imgURL} alt="randomly generated meme"/>
         </div>
     )
